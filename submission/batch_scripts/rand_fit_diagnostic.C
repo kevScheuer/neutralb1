dfc31f572cafcded9301e46534f5bc3e49f562d6
kevScheuer <kscheuer@jlab.org>
2024-08-07 11:20:38 -0400

    Scaled chi2 and plotted on log-log scale
    Chi2 value is now "reduced" i.e. divided by the
    total number of free parameters

--- submission/batch_scripts/rand_fit_diagnostic.C
+++ submission/batch_scripts/rand_fit_diagnostic.C
@@ -2,7 +2,11 @@
 
 Plot the sum of squares of error (with respect to the best fit) divided by the MINUIT
 uncertainty squared for every production and fit parameter, as a function of the
-difference in likelihood between each fit and the best fit.
+difference in likelihood between each fit and the best fit. Plotted on log-log scale
+
+NOTE: Once ROOT is updated so that TScatter is available, this script will become much
+simpler. Currently has a lot of hacky ways to get around not being able to natively
+make a scatter plot
 */
 #include <cmath>
 #include <dirent.h>
@@ -28,14 +32,23 @@ void rand_fit_diagnostic(std::string path, std::string reaction = "omegapi")
         std::cout << "Invalid fit results in file: " << path + "best.fit" << "\n";
         exit(1);
     }
+    // get number of fit parameters by assuming any errors=0 means the param is fixed
+    int num_free_params = 0;
+    for (std::string name : best_fit.parNameList())
+    {
+        if (best_fit.parError(name) != 0)
+            num_free_params += 1;
+    }
 
     double best_likelihood = best_fit.likelihood(); // min likelihood to compare to
     double convergence_rate = 0;                    // tracks percentage fits that converged successfully
-    TNtuple *tp = new TNtuple("tp", "tp", "delta_l:chi2");
+
     // params for making the plot later
     float p[2];
     float l_max = 0;
-    float chi2_min = std::pow(10, 10), chi2_max = 0;
+    float chi2_max = 0;
+
+    TNtuple *tp = new TNtuple("tp", "tp", "delta_l:chi2");
 
     for (std::string file : file_list)
     {
@@ -57,6 +70,7 @@ void rand_fit_diagnostic(std::string path, std::string reaction = "omegapi")
         // get a nice positive # for plotting
         float delta_l = rand_fit.likelihood() - best_likelihood;
         p[0] = delta_l;
+
         if (delta_l > l_max)
             l_max = delta_l;
 
@@ -76,24 +90,33 @@ void rand_fit_diagnostic(std::string path, std::string reaction = "omegapi")
             double best_par_val = best_fit.parValue(best_par_name);
             double best_par_err = best_fit.parError(best_par_name);
 
-            if (best_par_err == 0) // avoid discontinuity
+            if (best_par_err == 0) // avoid discontinuity from fixed params
                 continue;
 
             chi2 += std::pow((best_par_val - rand_par_val) / best_par_err, 2);
         }
 
-        p[1] = chi2;
-        tp->Fill(p);
-        if (chi2 > chi2_max)
-            chi2_max = chi2;
-        if (chi2 < chi2_min)
-            chi2_min = chi2;
+        float reduced_chi2 = chi2 / num_free_params;
+        p[1] = reduced_chi2;
+        tp->Fill(p);        
+
+        if (reduced_chi2 > chi2_max)
+            chi2_max = reduced_chi2;
     }
 
+    // convergence rate has count of successful fits, so divide by total # of rand fits
     convergence_rate = convergence_rate / file_list.size();
 
+    // avoid plotting issues if all rand fits at best value
+    if (l_max == 0.0)
+        l_max = 0.001;
+    if (chi2_max == 0.0)
+        chi2_max = 0.001;
+
     auto c1 = new TCanvas();
-    TH2F *frame = new TH2F("frame", "frame", 100, 0, l_max, 100, chi2_min, chi2_max);
+    c1->SetLogx(1);
+    c1->SetLogy(1);
+    TH2F *frame = new TH2F("frame", "frame", 100, 0, l_max, 100, 0, chi2_max);
     frame->GetXaxis()->SetTitle("\\Delta (-2ln L)");
     frame->GetYaxis()->SetTitle("\\chi^{2}");
     frame->Draw();
