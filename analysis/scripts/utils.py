46564c6dcc10c7683f9ef6bf7b4b5457a11b2ccf
kevScheuer <kscheuer@jlab.org>
2025-07-01 11:55:16 -0400

    Added shell env script utility function

--- analysis/scripts/utils.py
+++ analysis/scripts/utils.py
@@ -1,6 +1,13 @@
-"""A collection of some utility functions used in the analysis scripts."""
+"""A collection of some utility functions used in the analysis scripts.
 
+These are scripts that are not uniquely useful for partial wave analysis, and so kept
+seperate from pwa_tools.
+"""
+
+import os
+import pathlib
 import re
+import subprocess
 
 
 def sort_input_files(input_files: list, position: int = -1) -> list:
@@ -22,3 +29,37 @@ def sort_input_files(input_files: list, position: int = -1) -> list:
         return float(numbers[position]) if numbers else float("inf")
 
     return sorted(input_files, key=extract_last_number)
+
+
+def load_shell_environment() -> None:
+    """Load the shell environment variables from the setup script.
+
+    This function executes the `setup_gluex.sh` script in a bash shell and captures the
+    environment variables it sets. It then updates the current Python environment with
+    these variables. Particularly useful for jupyter notebook sessions.
+    """
+
+    # Load the environment variables from the shell setup script
+    root_dir = pathlib.Path(__file__).parent.parent.parent
+    command = f"bash -l -c 'source {root_dir}/setup_gluex.sh && env'"
+    proc = subprocess.Popen(
+        command, stdout=subprocess.PIPE, shell=True, executable="/bin/bash"
+    )
+    output, _ = proc.communicate()
+    # Parse the environment variables
+    env_vars = {}
+    for line in output.decode().splitlines():
+        # the output contains a bunch of BASH_FUNCS that will ruin the environment
+        # variables. this avoids those issues
+        if (
+            len(line.split("=", 1)) != 2
+            or line.startswith("BASH_FUNC")
+            or line.startswith(" ")
+            or line.startswith("\t")
+        ):
+            continue
+        key, value = line.split("=", 1)
+        env_vars[key] = value
+    os.environ.update(env_vars)
+
+    return
