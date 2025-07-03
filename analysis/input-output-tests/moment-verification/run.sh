69aa18da73b629527a94d805c3564e90d838561e
kevScheuer <kscheuer@jlab.org>
2025-07-02 22:36:58 -0400

    Update run methods
    Updates include:
    - Skipping data generation if already done
    - Randomize the amplitude-based fits
    - Move fit outputs into their own directories

--- analysis/input-output-tests/moment-verification/run.sh
+++ analysis/input-output-tests/moment-verification/run.sh
@@ -1,48 +1,70 @@
 #!/bin/bash
 # Perform the generation of signal MC, and fit with amplitudes and moments to it
-# NOTE: this assumes you are on a GPU node
+# NOTE: this assumes you are on a GPU node, and are running from the same directory as
+# the script
 
 WORKSPACE_DIR="/home/kscheuer/work/neutralb1/"
 source $WORKSPACE_DIR/setup_gluex.sh
 
 
 # GENERATE MC
-gen_vec_ps -c vec_ps_moment.cfg\
-    -o data.root\
-    -l 1.20 -u 1.22\
-    -n 50000\
-    -a 8.2 -b 8.8\
-    -tmin 0.1 -tmax 0.2
 if [ -e "data.root" ]; then
-    echo "Data generation successful."
+    echo "Data file already exists, skipping generation."
 else
-    echo "Data generation failed."
-    exit 1
+    echo "Generating data..."
+    gen_vec_ps -c amplitudes.cfg\
+        -o data.root\
+        -l 1.20 -u 1.22\
+        -n 50000\
+        -a 8.2 -b 8.8\
+        -tmin 0.1 -tmax 0.2
+    if [ -e "data.root" ]; then
+        echo "Data generation successful."
+    else
+        echo "Data generation failed."
+        exit 1
+    fi
 fi
 
 # FIT WITH AMPLITUDES
-fit -c amplitudes.cfg -m 10000000 > amplitude_fit.log
+fit -c amplitudes.cfg -m 10000000 -r 50 > amplitude_fit.log
 if [ -e "omegapi.fit" ]; then
-    echo "Fit successful."
-    mv omegapi.fit amplitude_result.fit
+    echo "Fit successful."    
 else
     echo "Fit failed."
     exit 1
 fi
-vecps_plotter amplitude_result.fit    
-$WORKSPACE_DIR/submission/batch_script/angle_plotter vecps_plot.root "Thrown MC" "omegapi" ./ true
-python $WORKSPACE_DIR/submission/batch_scripts/convert_to_csv.py -i amplitude_result.fit -o result.csv
-python $WORKSPACE_DIR/submission/batch_scripts/project_moments.py -i amplitude_result.fit -o projected_moments.csv
+vecps_plotter omegapi.fit    
+$WORKSPACE_DIR/submission/batch_scripts/angle_plotter vecps_plot.root "Thrown MC" "" ./ true
+python $WORKSPACE_DIR/submission/batch_scripts/convert_to_csv.py -i omegapi.fit -o result.csv
+python $WORKSPACE_DIR/submission/batch_scripts/project_moments.py\
+    -i omegapi.fit -o projected_moments.csv -m 1.21
+
+mkdir -p amplitude_results
+mv omegapi_*.fit amplitude_results/
+mv omegapi.fit amplitude_results/
+mv omegapi.ni amplitude_results/
+mv vecps_plot.root amplitude_results/
+mv vecps_fitPars.txt amplitude_results/
+mv result.csv amplitude_results/
+mv projected_moments.csv amplitude_results/
 
 # FIT WITH MOMENTS
-fit -c moments.cfg -m 10000000 > moment_fit.log
+fit -c moments.cfg -m 10000000 -r 50 > moment_fit.log
 if [ -e "omegapi.fit" ]; then
-    echo "Fit successful."
-    mv omegapi.fit moment_result.fit
+    echo "Fit successful."    
 else
     echo "Fit failed."
     exit 1
 fi
-vecps_plotter moment_result.fit
-$WORKSPACE_DIR/submission/batch_script/angle_plotter vecps_plot.root "Thrown MC" "omegapi" ./ true
-python $WORKSPACE_DIR/submission/batch_scripts/convert_to_csv.py -i moment_result.fit -o result_moments.csv
+vecps_plotter omegapi.fit
+$WORKSPACE_DIR/submission/batch_script/angle_plotter vecps_plot.root "Thrown MC" "" ./ true
+python $WORKSPACE_DIR/submission/batch_scripts/convert_to_csv.py -i omegapi.fit -o result.csv
+
+mkdir -p moment_results
+mv omegapi_*.fit moment_results/
+mv omegapi.fit moment_results/
+mv omegapi.ni moment_results/
+mv vecps_plot.root moment_results/
+mv vecps_fitPars.txt moment_results/
+mv result.csv moment_results/
