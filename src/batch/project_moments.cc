ba7196506eb6beacf6d47ccdfb37121f72b35bf7
kevScheuer <kscheuer@jlab.org>
2025-07-08 21:42:48 -0400

    Change of file methods
    There is no need to mimic the python version, as
    that heavily depended on reading the fit result
    once. Here we can access them with AmpTools, and
    so can heavily restructure how the moments will
    be calculated.

--- src/batch/project_moments.cc
+++ src/batch/project_moments.cc
@@ -13,7 +13,7 @@ TODO: Fill in
 TODO: Print a warning to the user if the fit results do not contain the same set of
     amplitudes. Have default behavior fill in the missing amplitudes with 0.
 TODO: The current way is to get the production coefficient first, store them, then later
-    fill them in when calculating the sdme. It might be easier to simply store the 
+    fill them in when calculating the sdme. It might be easier to simply store the
     amplitude name then call the scaled production coefficient later?
 Usage: project_moments
 */
@@ -30,10 +30,19 @@ Usage: project_moments
 #include "file_utils.h"
 #include "amp_utils.h"
 
-
+struct Moment
+{
+    int alpha;
+    int Jv;
+    int Lambda;
+    int J;
+    int M;
+};
 
 // forward declarations
-std::map<std::string, double> calculate_moments(const FitResults &results);
+std::vector<Moment> initialize_moments(const FitResults &results);
+int find_max_m(const FitResults &results);
+int find_max_J(const FitResults &results);
 
 int main(int argc, char *argv[])
 {
@@ -56,7 +65,8 @@ int main(int argc, char *argv[])
     // and write them to the csv file.
 
     // initialize the map of moment names to their values
-    std::map<std::string, double> moments;
+    std::map<std::string, double> moment_results;
+    std::vector<Moment> moments; // vector of all moments to be calculated
 
     // open csv file for writing
     std::ofstream csv_file;
@@ -79,18 +89,68 @@ int main(int argc, char *argv[])
         }
 
         // before getting this file's info, clear the results from the last file
+        moment_results.clear();
         moments.clear();
 
-        // calculate the moments from the fit results
-        moments = calculate_moments(results);
+        // initialize the set of moments we can have from this file's waveset
+        moments = initialize_moments(results);
 
     } // end of file iteration
 }
 
-std::map<std::string, double> calculate_moments(const FitResults &results)
+std::vector<Moment> initialize_moments(const FitResults &results)
 {
-    std::map<std::string, double> moments;
+    std::vector<Moment> moments;
+    int max_J = find_max_J(results);
+    int max_m = find_max_m(results);
 
+    // prepare moment quantum numbers for the moments
+    std::vector<int> alpha_vector = {0, 1, 2}; 
+    std::vector<int> Jv_vector = {0, 2}; // CGs coefficient ensure Jv=1 is always 0
+    // moments with negative Lambda values are proportional to positive ones
+    std::vector<int> Lambda_vector = {0, 1, 2};
+    std::vector<int> J_vector;
+    for (int J = 0; J <= max_J; ++J) // J defined to be >= 0
+    {
+        J_vector.push_back(J);
+    }
+    std::vector<int> M_vector; // similar to lambda, (-) M values ar proportional to (+)
+    for (int m = 0; m <= max_m; ++m)
+    {
+        M_vector.push_back(m);
+    }
+    for (int alpha : alpha_vector)
+    {
+        for (int Jv : Jv_vector)
+        {
+            for (int Lambda : Lambda_vector)
+            {
+                for (int J : J_vector)
+                {
+                    for (int M : M_vector)
+                    {
+                        // FILTER non-physical moments
+                        // Wigner D functions for these are always 0
+                        if (M>J || Lambda > J || Lambda > Jv)
+                            continue;
+                        // Wishart seciton 5.10.2 proves H2(Jv,0,J,0) = 0 for any Jv, J
+                        if (alpha == 2 && Lambda == 0 && M == 0)
+                            continue;
+
+                        // create a moment from these quantum numbers
+                        Moment moment;
+                        moment.alpha = alpha;
+                        moment.Jv = Jv;
+                        moment.Lambda = Lambda;
+                        moment.J = J;
+                        moment.M = M;
+
+                        moments.push_back(moment);
+                    }
+                }
+            }
+        }
+    }
     return moments;
 }
 
@@ -110,4 +170,22 @@ int find_max_m(const FitResults &results)
     }
 
     return max_m;
+}
+
+int find_max_J(const FitResults &results)
+{
+    int max_J = 0;
+    for (const auto &reaction : results.reactionList())
+    {
+        for (const std::string &amplitude : results.ampList(reaction))
+        {
+            int J_value = std::stoi(parse_amplitude(amplitude).J);
+            if (J_value > max_J)
+            {
+                max_J = J_value;
+            }
+        }
+    }
+
+    return max_J;
 }
