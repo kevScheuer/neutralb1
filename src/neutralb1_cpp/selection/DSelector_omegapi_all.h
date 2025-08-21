f6a801265476389ba83215efa08c06785eaf7cb8
kevScheuer <kscheuer@jlab.org>
2025-08-21 10:34:08 -0400

    formatting

--- src/neutralb1_cpp/selection/DSelector_omegapi_all.h
+++ src/neutralb1_cpp/selection/DSelector_omegapi_all.h
@@ -12,83 +12,81 @@
 
 class DSelector_omegapi_all : public DSelector
 {
-	public:
+public:
+	DSelector_omegapi_all(TTree *locTree = NULL) : DSelector(locTree) {}
+	virtual ~DSelector_omegapi_all() {}
 
-		DSelector_omegapi_all(TTree* locTree = NULL) : DSelector(locTree){}
-		virtual ~DSelector_omegapi_all(){}
+	void Init(TTree *tree);
+	Bool_t Process(Long64_t entry);
 
-		void Init(TTree *tree);
-		Bool_t Process(Long64_t entry);
+private:
+	void Get_ComboWrappers(void);
+	void Finalize(void);
 
-	private:
+	// BEAM POLARIZATION INFORMATION
+	UInt_t dPreviousRunNumber;
+	bool dIsPolarizedFlag; // else is AMO
+	bool dIsPARAFlag;	   // else is PERP or AMO
 
-		void Get_ComboWrappers(void);
-		void Finalize(void);
+	bool dIsMC;
 
-		// BEAM POLARIZATION INFORMATION
-		UInt_t dPreviousRunNumber;
-		bool dIsPolarizedFlag; //else is AMO
-		bool dIsPARAFlag; //else is PERP or AMO
+	TH1F *dHist_KinFitChiSq;
+	TH1F *dHist_4piMassSum, *dHist_3piMassSum, *dHist_3piMassSumMatched, *dHist_2gammaMassSum;
+	TH2F *dHist_3piMassCorrWeight, *dHist_3piMassCorr[10], *dHist_3piMassCorr2DWeight[10];
 
-		bool dIsMC;
+	TH2F *dHist_cosTheta_4piMass, *dHist_phi_4piMass;
+	TH2F *dHist_cosThetaH_4piMass, *dHist_phiH_4piMass;
+	TH2F *dHist_lambda_4piMass;
 
-		TH1F *dHist_KinFitChiSq;
-                TH1F *dHist_4piMassSum, *dHist_3piMassSum, *dHist_3piMassSumMatched, *dHist_2gammaMassSum;
-                TH2F *dHist_3piMassCorrWeight, *dHist_3piMassCorr[10], *dHist_3piMassCorr2DWeight[10];
+	// CREATE REACTION-SPECIFIC PARTICLE ARRAYS
 
-		TH2F *dHist_cosTheta_4piMass, *dHist_phi_4piMass;
-		TH2F *dHist_cosThetaH_4piMass, *dHist_phiH_4piMass;
-		TH2F *dHist_lambda_4piMass;
+	// Step 0
+	DParticleComboStep *dStep0Wrapper;
+	DBeamParticle *dComboBeamWrapper;
+	DChargedTrackHypothesis *dPiPlusWrapper;
+	DChargedTrackHypothesis *dPiMinusWrapper;
+	DChargedTrackHypothesis *dProtonWrapper;
 
-		//CREATE REACTION-SPECIFIC PARTICLE ARRAYS
+	// Step 1
+	DParticleComboStep *dStep1Wrapper;
+	DKinematicData *dDecayingPi01Wrapper;
+	DNeutralParticleHypothesis *dPhoton1Wrapper;
+	DNeutralParticleHypothesis *dPhoton2Wrapper;
 
-		//Step 0
-		DParticleComboStep* dStep0Wrapper;
-		DBeamParticle* dComboBeamWrapper;
-		DChargedTrackHypothesis* dPiPlusWrapper;
-		DChargedTrackHypothesis* dPiMinusWrapper;
-		DChargedTrackHypothesis* dProtonWrapper;
+	// Step 2
+	DParticleComboStep *dStep2Wrapper;
+	DKinematicData *dDecayingPi02Wrapper;
+	DNeutralParticleHypothesis *dPhoton3Wrapper;
+	DNeutralParticleHypothesis *dPhoton4Wrapper;
 
-		//Step 1
-		DParticleComboStep* dStep1Wrapper;
-		DKinematicData* dDecayingPi01Wrapper;
-		DNeutralParticleHypothesis* dPhoton1Wrapper;
-		DNeutralParticleHypothesis* dPhoton2Wrapper;
-
-		//Step 2
-		DParticleComboStep* dStep2Wrapper;
-		DKinematicData* dDecayingPi02Wrapper;
-		DNeutralParticleHypothesis* dPhoton3Wrapper;
-		DNeutralParticleHypothesis* dPhoton4Wrapper;
-
-		// DEFINE YOUR HISTOGRAMS HERE
-		// EXAMPLES:
-		TH1I* dHist_MissingMassSquared;
-		TH1I* dHist_BeamEnergy;
+	// DEFINE YOUR HISTOGRAMS HERE
+	// EXAMPLES:
+	TH1I *dHist_MissingMassSquared;
+	TH1I *dHist_BeamEnergy;
 
 	ClassDef(DSelector_omegapi_all, 0);
 };
 
 void DSelector_omegapi_all::Get_ComboWrappers(void)
 {
-	//Step 0
+	// Step 0
 	dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
-	dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
-	dPiPlusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(2));
-	dPiMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(3));
-	dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(4));
+	dComboBeamWrapper = static_cast<DBeamParticle *>(dStep0Wrapper->Get_InitialParticle());
+	dPiPlusWrapper = static_cast<DChargedTrackHypothesis *>(dStep0Wrapper->Get_FinalParticle(2));
+	dPiMinusWrapper = static_cast<DChargedTrackHypothesis *>(dStep0Wrapper->Get_FinalParticle(3));
+	dProtonWrapper = static_cast<DChargedTrackHypothesis *>(dStep0Wrapper->Get_FinalParticle(4));
 
-	//Step 1
+	// Step 1
 	dStep1Wrapper = dComboWrapper->Get_ParticleComboStep(1);
 	dDecayingPi01Wrapper = dStep1Wrapper->Get_InitialParticle();
-	dPhoton1Wrapper = static_cast<DNeutralParticleHypothesis*>(dStep1Wrapper->Get_FinalParticle(0));
-	dPhoton2Wrapper = static_cast<DNeutralParticleHypothesis*>(dStep1Wrapper->Get_FinalParticle(1));
+	dPhoton1Wrapper = static_cast<DNeutralParticleHypothesis *>(dStep1Wrapper->Get_FinalParticle(0));
+	dPhoton2Wrapper = static_cast<DNeutralParticleHypothesis *>(dStep1Wrapper->Get_FinalParticle(1));
 
-	//Step 2
+	// Step 2
 	dStep2Wrapper = dComboWrapper->Get_ParticleComboStep(2);
 	dDecayingPi02Wrapper = dStep2Wrapper->Get_InitialParticle();
-	dPhoton3Wrapper = static_cast<DNeutralParticleHypothesis*>(dStep2Wrapper->Get_FinalParticle(0));
-	dPhoton4Wrapper = static_cast<DNeutralParticleHypothesis*>(dStep2Wrapper->Get_FinalParticle(1));
+	dPhoton3Wrapper = static_cast<DNeutralParticleHypothesis *>(dStep2Wrapper->Get_FinalParticle(0));
+	dPhoton4Wrapper = static_cast<DNeutralParticleHypothesis *>(dStep2Wrapper->Get_FinalParticle(1));
 }
 
 #endif // DSelector_omegapi_all_h
--- src/neutralb1_cpp/selection/runSelector.C
+++ src/neutralb1_cpp/selection/runSelector.C
@@ -1,5 +1,5 @@
 // macro to process analysis TTree with TSelector
-#include <iostream> 
+#include <iostream>
 
 #include "DSelector/DPROOFLiteManager.h"
 #include "TChain.h"
@@ -13,54 +13,60 @@
 
 R__LOAD_LIBRARY(libDSelector.so)
 
-void runSelector(TString runNumber = "30496", TString myPath = "/sciclone/gluex10/RunPeriod-2017-01/analysis/ver35/tree_pi0pi0pippim__B4/merged/", TString myOption = "") 
+void runSelector(TString runNumber = "30496", TString myPath = "/sciclone/gluex10/RunPeriod-2017-01/analysis/ver35/tree_pi0pi0pippim__B4/merged/", TString myOption = "")
 
 {
-  // Load DSelector library
-  gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
-  int Proof_Nthreads = 8;
+	// Load DSelector library
+	gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
+	int Proof_Nthreads = 8;
 
-  // process signal 
-  TString sampleDir = myPath;
-  std::cout<<"running selector on files in: "<<sampleDir.Data() << "\n";
-  
-  TString treeName = "pi0pi0pippim__B4_Tree";
-  if(myPath.Contains("tree_thrown")) treeName = "Thrown_Tree";
-  TChain *chain = new TChain(treeName); 
-  TSystemDirectory dir(sampleDir, sampleDir);
-  TList *files = dir.GetListOfFiles();
-  int ifile = 0;
-  if(files) {
-	  TSystemFile *file;
-	  TString fileName;
-	  TIter next(files);
-	  
-	  // loop over files
-	  while ((file=(TSystemFile*)next())) {
-		  fileName = file->GetName();
-		  if(fileName.Contains(runNumber)) {
-			  std::cout<<fileName.Data()<< "\n";
-			  
-			  // check if file corrupted
-			  TFile f(sampleDir+fileName);
-			  if(f.TestBit(TFile::kRecovered)) {
-				  std::cout<<"file corrupted -> skipping"<< "\n";
-				  continue;
-			  }
-			  if(f.IsZombie()) {
-				  std::cout<<"file is a Zombie -> skipping"<< "\n";
-				  continue;
-			  }
-			  
-			  // add file to chain
-			  chain->Add(sampleDir+fileName);
-			  ifile++;
-		  }
-	  }
+	// process signal
+	TString sampleDir = myPath;
+	std::cout << "running selector on files in: " << sampleDir.Data() << "\n";
 
-	  std::cout<<"total entries in TChain = "<<chain->GetEntries()<<" from "<<ifile<<" files"<<endl;
-	  DPROOFLiteManager::Process_Chain(chain, "DSelector_omegapi_all.C++", Proof_Nthreads, Form("hist_pomegapi_omega3pi_%s.acc.root", runNumber.Data()), Form("tree_pomegapi_omega3pi_%s.acc.root", runNumber.Data()), Form("%s DEFAULTFLATOFF",myOption.Data()));
-  }
+	TString treeName = "pi0pi0pippim__B4_Tree";
+	if (myPath.Contains("tree_thrown"))
+		treeName = "Thrown_Tree";
+	TChain *chain = new TChain(treeName);
+	TSystemDirectory dir(sampleDir, sampleDir);
+	TList *files = dir.GetListOfFiles();
+	int ifile = 0;
+	if (files)
+	{
+		TSystemFile *file;
+		TString fileName;
+		TIter next(files);
 
-  return;
+		// loop over files
+		while ((file = (TSystemFile *)next()))
+		{
+			fileName = file->GetName();
+			if (fileName.Contains(runNumber))
+			{
+				std::cout << fileName.Data() << "\n";
+
+				// check if file corrupted
+				TFile f(sampleDir + fileName);
+				if (f.TestBit(TFile::kRecovered))
+				{
+					std::cout << "file corrupted -> skipping" << "\n";
+					continue;
+				}
+				if (f.IsZombie())
+				{
+					std::cout << "file is a Zombie -> skipping" << "\n";
+					continue;
+				}
+
+				// add file to chain
+				chain->Add(sampleDir + fileName);
+				ifile++;
+			}
+		}
+
+		std::cout << "total entries in TChain = " << chain->GetEntries() << " from " << ifile << " files" << endl;
+		DPROOFLiteManager::Process_Chain(chain, "DSelector_omegapi_all.C++", Proof_Nthreads, Form("hist_pomegapi_omega3pi_%s.acc.root", runNumber.Data()), Form("tree_pomegapi_omega3pi_%s.acc.root", runNumber.Data()), Form("%s DEFAULTFLATOFF", myOption.Data()));
+	}
+
+	return;
 }
