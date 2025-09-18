e93e67df227672c3a364dac8ca9918fce4225a93
kevScheuer <kscheuer@jlab.org>
2025-09-18 14:21:47 -0400

    added preview mode

--- scripts/collect_all_fits.bash
+++ scripts/collect_all_fits.bash
@@ -9,17 +9,22 @@
 #           - randomized fit results
 
 usage() {
-    echo "Usage: collect_all_fits.bash -d <input_directory> -o <output_directory>"
+    echo "Usage: collect_all_fits.bash -d <input_directory> -o <output_directory> [-p] [-h]"
     echo "  -d: -t bin directory containing subdirectories with fit results"
     echo "  -o: Output directory to store collected csv files"
+    echo "  -p: Preview mode (do not execute commands)"
     echo "  -h: Display this help message"    
 }
 
-while getopts ":d:o:h" opt; do
+preview_mode="false"
+
+while getopts ":d:o:ph" opt; do
     case $opt in
         d) input_dir="$OPTARG"
         ;;
         o) output_dir="$OPTARG"
+        ;;
+        p) preview_mode="true"
         ;;
         h) 
             usage
@@ -50,49 +55,67 @@ if [ ${#mass_dirs[@]} -eq 0 ]; then
     exit 1
 fi
 
+# use preview flag to printout what files will be combined if in preview mode
+if [ "$preview_mode" == "true" ]; then
+    echo "Preview mode enabled. The following files will be combined:"
+    preview="-p"
+else
+    preview=""
+fi
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/data.csv -o ${output_dir}/data.csv
-python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best.csv -o ${output_dir}/best.csv
-python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best_acceptance_corrected.csv -o ${output_dir}/best_acceptance_corrected.csv
+python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/data.csv -o ${output_dir}/data.csv $preview
+python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best.csv -o ${output_dir}/best.csv $preview
+python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best_acceptance_corrected.csv -o ${output_dir}/best_acceptance_corrected.csv $preview
 
 # handle optional files
-if [ -f ${input_dir}/mass*/best_projected_moments.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best_projected_moments.csv -o ${output_dir}/moments.csv
+files=(${input_dir}/mass*/best_projected_moments.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/best_projected_moments.csv -o ${output_dir}/moments.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/rand/rand.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand.csv -o ${output_dir}/rand.csv
+files=(${input_dir}/mass*/rand/rand.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand.csv -o ${output_dir}/rand.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/rand/rand_acceptance_corrected.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand_acceptance_corrected.csv -o ${output_dir}/rand_acceptance_corrected.csv
+files=(${input_dir}/mass*/rand/rand_acceptance_corrected.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand_acceptance_corrected.csv -o ${output_dir}/rand_acceptance_corrected.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/rand/rand_projected_moments.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand_projected_moments.csv -o ${output_dir}/rand_moments.csv
+files=(${input_dir}/mass*/rand/rand_projected_moments.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/rand/rand_projected_moments.csv -o ${output_dir}/rand_moments.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/bootstrap/bootstrap.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap.csv -o ${output_dir}/bootstrap.csv
+files=(${input_dir}/mass*/bootstrap/bootstrap.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap.csv -o ${output_dir}/bootstrap.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/bootstrap/bootstrap_acceptance_corrected.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap_acceptance_corrected.csv -o ${output_dir}/bootstrap_acceptance_corrected.csv
+files=(${input_dir}/mass*/bootstrap/bootstrap_acceptance_corrected.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap_acceptance_corrected.csv -o ${output_dir}/bootstrap_acceptance_corrected.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/bootstrap/bootstrap_projected_moments.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap_projected_moments.csv -o ${output_dir}/bootstrap_moments.csv
+files=(${input_dir}/mass*/bootstrap/bootstrap_projected_moments.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/bootstrap/bootstrap_projected_moments.csv -o ${output_dir}/bootstrap_moments.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/truth/best.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best.csv -o ${output_dir}/truth.csv
+files=(${input_dir}/mass*/truth/best.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best.csv -o ${output_dir}/truth.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/truth/best.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best_acceptance_corrected.csv -o ${output_dir}/truth_acceptance_corrected.csv
+files=(${input_dir}/mass*/truth/best_acceptance_corrected.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best_acceptance_corrected.csv -o ${output_dir}/truth_acceptance_corrected.csv $preview
 fi
 
-if [ -f ${input_dir}/mass*/truth/best_projected_moments.csv ]; then
-    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best_projected_moments.csv -o ${output_dir}/truth_projected_moments.csv
+files=(${input_dir}/mass*/truth/best_projected_moments.csv)
+if [ ${#files[@]} -gt 0 ]; then
+    python ${script_dir}/collect_csv.py -i ${input_dir}/mass*/truth/best_projected_moments.csv -o ${output_dir}/truth_projected_moments.csv $preview
 fi
