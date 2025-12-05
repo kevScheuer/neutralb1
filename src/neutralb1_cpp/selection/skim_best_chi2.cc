b5fef91c0e63b6782d9513fc90672e21167e377b
kevScheuer <kscheuer@jlab.org>
2025-12-05 11:57:25 -0500

    add gen phasespace skimming

--- src/neutralb1_cpp/selection/skim_best_chi2.cc
+++ src/neutralb1_cpp/selection/skim_best_chi2.cc
@@ -28,6 +28,7 @@ void skim_best_chi2(int period)
     // signal includes thrown and reconstructed
     TString signal_mc_files = TString::Format("/cache/halld/home/jrsteven/flattened/omegapi_gx1_pwa/ver01/tree_pi0pi0pippim__B4/omegapi_massDepFit_201*ver03.1/tree_pi0pi0pippim__B4_FSROOT_0%i*.root", period);
     TString phasespace_mc_files = TString::Format("/cache/halld/home/jrsteven/flattened/omegapi_gx1_pwa/ver01/tree_pi0pi0pippim__B4/omegapi_phasespace*ver03/tree_pi0pi0pippim__B4_FSROOT_0%i*.root", period);
+    TString gen_phasespace_mc_files = TString::Format("/cache/halld/home/jrsteven/flattened/omegapi_gx1_pwa/ver01/tree_pi0pi0pippim__B4/omegapi_phasespace*ver03/tree_thrown_FSROOT_MCGEN_0%i*.root", period);
 
     setup(false);
 
@@ -37,9 +38,16 @@ void skim_best_chi2(int period)
     FSCut::defineCut("eBeam", "(EnPB>8.2&&EnPB<8.8)");
     TString skim_cuts = "chi2,eBeam";
 
+    // The gen phasespace files only need eBeam, -t, and omega pi0 mass cut. We will 
+    // implement those early here, because we know what ranges we're going to use.
+    FSCut::defineCut("tcut", "abs(-1*MCMASS2(1,-GLUEXTARGET))<1.0");
+    FSCut::defineCut("omegapi0mass", "MCMASS(2,3,4,5)>=1.0&&MCMASS(2,3,4,5)<=2.0");
+    FSCut::defineCut("eBeamGen", "(MCEnPB>8.2&&MCEnPB<8.8)");    
+
     TString output_data = TString::Format("/lustre24/expphy/volatile/halld/home/kscheuer/FSRoot-skimmed-trees/general/tree_pi0pi0pippim__B4_GENERAL_SKIM_0%i_data.root", period);
     TString output_signal_mc = TString::Format("/lustre24/expphy/volatile/halld/home/kscheuer/FSRoot-skimmed-trees/general/tree_pi0pi0pippim__B4_GENERAL_SKIM_0%i_ver03.1_mc.root", period);
     TString output_phasespace_mc = TString::Format("/lustre24/expphy/volatile/halld/home/kscheuer/FSRoot-skimmed-trees/general/tree_pi0pi0pippim__B4_GENERAL_SKIM_0%i_ver03.root", period);
+    TString output_gen_phasespace_mc = TString::Format("/lustre24/expphy/volatile/halld/home/kscheuer/FSRoot-skimmed-trees/general/tree_pi0pi0pippim__B4_GENERAL_SKIM_0%i_ver03_gen.root", period);
 
     // Now skim the trees and save to new files
     FSModeTree::skimTree(
@@ -60,6 +68,12 @@ void skim_best_chi2(int period)
         CATEGORY,
         output_phasespace_mc,
         TString::Format("CUT(%s)", skim_cuts.Data()));
+    FSModeTree::skimTree(
+        gen_phasespace_mc_files,
+        NT,
+        CATEGORY,
+        output_gen_phasespace_mc,
+        "CUT(eBeamGen,tcut,omegapi0mass)");
 
     // Following the doc from above, we'll group by run, event, and beam energy to
     // create chi2 ranked trees
